2546e6004157386c8df53778766f6bb25ecdd530
it63070098 <74039435+it63070098@users.noreply.github.com>
2020-11-13 15:12:02 +0700

    Update showw.php

--- showw.php
+++ showw.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'butsayamas.mysql.database.azure.com', 'K63070098@butsayamas', 'Khaw63070098', 'itflab1', guestbook3);
+mysqli_real_connect($conn, 'butsayamas.mysql.database.azure.com', 'K63070098@butsayamas', 'Khaw63070098', 'itflab1', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
