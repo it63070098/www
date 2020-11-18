b4a86c7fb031f4d0621b105f3c4eb9f9d3383ede
it63070098 <74039435+it63070098@users.noreply.github.com>
2020-11-18 19:22:21 +0700

    Update showw.php

--- showw.php
+++ showw.php
@@ -16,13 +16,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook3');
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Action </div>
-      <div class="container">
-  <h2>Button Elements</h2>
-  <a href="#" class="btn btn-info" role="button">Link Button</a>
-  <button type="button" class="btn btn-info">Button</button>
-  <input type="button" class="btn btn-info" value="Input Button">
-  <input type="submit" class="btn btn-info" value="Submit Button"></div></th>
+    <th width="150"> <div align="center">Action </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
@@ -31,7 +25,13 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Action'];?></td>
+    <td><?php echo $Result['Action'];?>
+        <div class="container">
+  <h2>Button Elements</h2>
+  <a href="#" class="btn btn-info" role="button">Link Button</a>
+  <button type="button" class="btn btn-info">Button</button>
+  <input type="button" class="btn btn-info" value="Input Button">
+  <input type="submit" class="btn btn-info" value="Submit Button"></div></td>
   </tr>
 <?php
 }
