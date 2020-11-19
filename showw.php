49eb4a944d4c59ac772ce09d08bf7256cec9cb42
it63070098 <74039435+it63070098@users.noreply.github.com>
2020-11-19 22:57:36 +0700

    Update showw.php

--- showw.php
+++ showw.php
@@ -1,7 +1,12 @@
 <html>
 <head>
+    <title>ITF Lab</title>
     <meta charset="UTF-8">
-<title>ITF Lab</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php
@@ -13,12 +18,14 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook3');
 ?>
-<table width="600" border="1">
+<table width="600" border="1" class="table">
+    <thead class="thead-dark">
   <tr><center>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
     <th width="150"> <div align="center">Action </div></th>
   </center></tr>
+  </thead>
 <?php
 while($Result = mysqli_fetch_array($res))
 {
