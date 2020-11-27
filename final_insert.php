eb3b6b5200806f1461ef0067490caabf2a1f77f3
it63070098 <74039435+it63070098@users.noreply.github.com>
2020-11-27 15:33:26 +0700

    Update final_insert.php

--- final_insert.php
+++ final_insert.php
@@ -1,10 +1,11 @@
 <?php
+
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'butsayamas.mysql.database.azure.com', 'K63070098@butsayamas', 'Khaw63070098', 'itflab1', 3306);
-if(mysqli_connect_errno($conn)){
-    echo "connect fail".mysqli_connect_error();
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-?>
 
     if(isset($_POST['submit'])){
         $product = $_POST['Product'];
